1ea72b241c0f8978f4c7e75240c073891a151fba
Caleb Jasik <calebjasik@jasik.xyz>
2025-01-28 17:32:26 -0600

    Fix len

--- cert/ca_pool_test.go
+++ cert/ca_pool_test.go
@@ -100,10 +100,10 @@ IBNWYMep3ysx9zCgknfG5dKtwGTaqF++BWKDYdyl34KX
 	assert.Equal(t, pppp.CAs[string("c9bfaf7ce8e84b2eeda2e27b469f4b9617bde192efd214b68891ecda6ed49522")].Certificate.Name(), rootCA.details.Name)
 	assert.Equal(t, pppp.CAs[string("5c9c3f23e7ee7fe97637cbd3a0a5b854154d1d9aaaf7b566a51f4a88f76b64cd")].Certificate.Name(), rootCA01.details.Name)
 	assert.Equal(t, "expired", pppp.CAs[string("152070be6bb19bc9e3bde4c2f0e7d8f4ff5448b4c9856b8eccb314fade0229b0")].Certificate.Name())
-	assert.Equal(t, 3, len(pppp.CAs))
+	assert.Len(t, pppp.CAs, 3)
 
 	ppppp, err := NewCAPoolFromPEM([]byte(p256))
 	assert.NoError(t, err)
 	assert.Equal(t, ppppp.CAs[string("a7938893ec8c4ef769b06d7f425e5e46f7a7f5ffa49c3bcf4a86b608caba9159")].Certificate.Name(), rootCAP256.details.Name)
-	assert.Equal(t, 1, len(ppppp.CAs))
+	assert.Len(t, ppppp.CAs, 1)
 }
